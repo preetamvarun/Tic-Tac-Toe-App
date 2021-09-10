138baf74fa6fdfee9def81ee8740dd0592c861fa
Preetam Varun <preetamarun99@gmail.com>
2021-09-10 12:34:25 +0530

    add todos in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/screens/welcome_screen.dart';
 import 'constants.dart';
 
+
+// TODO : Add SOUND WHEN USER TAPS ON THE WRAPPING CONTAINER.
+// TODO : X sound should be different from O sound.
+// TODO : play sound when the game has been won or drawn.
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
