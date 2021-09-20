b17551139c63f830302a9a9d7aa59a46d2839c60
Preetam Varun <preetamarun99@gmail.com>
2021-09-20 17:42:51 +0530

    While running the app restrict device orientation to potraint up and down

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/screens/welcome_screen.dart';
 import 'constants.dart';
+import 'package:flutter/services.dart';
 
 // TODO : COMPLETE THESE TODOS TO PUBLISH YOUR APP ON PLAY STORE AND APP STORE
 
-// TODO : USE FLEXIBLE WIDGET AND LAYOUT BUILDER TO MAKE UI MORE RESPONSIVE.
 // TODO : MAKE UI RESPONSIVE IN LANDSCAPE MODE ALSO OR RESTRICT THE APP FOR NORMAL MODE.
-// TODO : MAKE THE USER TO SET HIS AVATAR AND A NAME.
-// TODO : TOGGLE THEMES.
-// TODO : ALLOW PLAYERS TO PLAY ONLINE.
-// TODO : ADD BACKGROUND MUSIC.
 // TODO : IMPLEMENT PLAYER VS COMPUTER.
 // TODO : WHEN USER PLAYS PLAYER VS COMPUTER ADD THREE LEVELS. EASY,MEDIUM,HARD.
 
@@ -19,6 +15,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
         home: Scaffold(
           body: WelcomeScreen(),
